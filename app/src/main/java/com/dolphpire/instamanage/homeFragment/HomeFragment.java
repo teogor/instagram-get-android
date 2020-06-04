f11932bfa4333fa69a37057f0d7c6f0b511e774e
Teodor G <grigor.teodor@gmail.com>
2020-06-04 22:53:58 +0200

    home class supports fragments

--- app/src/main/java/com/dolphpire/instamanage/homeFragment/HomeFragment.java
+++ app/src/main/java/com/dolphpire/instamanage/homeFragment/HomeFragment.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dolphpire.instamanage.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
 
@@ -21,13 +27,22 @@ public class HomeFragment extends Fragment {
     private Context mContext;
     private Activity mActivity;
 
+    @BindView(R.id.btnLikes)
+    TextView btnLikes;
+    @BindView(R.id.btnFollowers)
+    TextView btnFollowers;
+    @BindView(R.id.btnCoins)
+    ImageView btnCoins;
+
     public HomeFragment() {
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, mView);
 
         mActivity = getActivity();
 
@@ -58,7 +73,33 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+        btnLikes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        btnFollowers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCoins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void showFragment(Fragment fragment) {
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 
 }
