9a30421ed65899177a3386e89f3d8f3a9a1f9fc1
Teodor G <grigor.teodor@gmail.com>
2020-06-04 21:38:39 +0200

    added drawer

--- app/src/main/java/com/dolphpire/instamanage/home/HomeActivity.java
+++ app/src/main/java/com/dolphpire/instamanage/home/HomeActivity.java
@@ -1,10 +1,27 @@
 package com.dolphpire.instamanage.home;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.dolphpire.instamanage.R;
+import com.dolphpire.instamanage.home.drawer.DrawerAdapter;
+import com.dolphpire.instamanage.home.drawer.DrawerItem;
+import com.dolphpire.instamanage.home.drawer.SimpleItem;
+import com.dolphpire.instamanage.home.drawer.SpaceItem;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +29,79 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.dolphpire.instamanage.R.layout.activity_home);
+        setDrawer(savedInstanceState);
     }
+
+    private int POS_FEED = 0;
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+    private SlidingRootNav slidingRootNav;
+    private DrawerAdapter adapter;
+    private void setDrawer(Bundle savedInstanceState) {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_home_drawer)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_FEED).setChecked(true)));
+
+        adapter.setListener(new DrawerAdapter.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int position) {
+
+            }
+        });
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+    }
+
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.colorUnselected))
+                .withTextTint(color(R.color.colorUnselected))
+                .withTextSize(14)
+                .withIconSize(40)
+                .withSelectedIconTint(color(R.color.colorSelected))
+                .withSelectedTextTint(color(R.color.colorSelected))
+                .withSelectedTextSize(16)
+                .withSelectedIconSize(48);
+    }
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.ld_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.ld_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
+    }
+
 }
