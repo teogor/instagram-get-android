06e621bd6638d585365da90e1300883897a3d7d8
Teodor G <grigor.teodor@gmail.com>
2020-06-05 17:47:37 +0200

    added toolbar

--- app/src/main/java/com/dolphpire/instamanage/igaccounts/IGAccountActivity.java
+++ app/src/main/java/com/dolphpire/instamanage/igaccounts/IGAccountActivity.java
@@ -1,17 +1,29 @@
 package com.dolphpire.instamanage.igaccounts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.dolphpire.instamanage.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class IGAccountActivity extends AppCompatActivity {
+
+    @BindView(R.id.imvBack)
+    ImageView imvBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ig_account);
+
+        ButterKnife.bind(this);
+
+        imvBack.setOnClickListener(view -> finish());
+
     }
 
     @Override
