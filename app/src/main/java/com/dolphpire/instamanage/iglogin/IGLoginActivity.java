3eb4556c44eac0900a3510afc9c23faa08e0d473
Teodor G <grigor.teodor@gmail.com>
2020-06-05 22:30:43 +0200

    system to validate the data added

--- app/src/main/java/com/dolphpire/instamanage/iglogin/IGLoginActivity.java
+++ app/src/main/java/com/dolphpire/instamanage/iglogin/IGLoginActivity.java
@@ -3,17 +3,37 @@ package com.dolphpire.instamanage.iglogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.dolphpire.instamanage.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.dolphpire.instamanage.utils.Utils.hideKeyboard;
 
 public class IGLoginActivity extends AppCompatActivity {
 
     @BindView(R.id.imvBack)
     ImageView imvBack;
+    @BindView(R.id.llLoginButton)
+    LinearLayout llLoginButton;
+    @BindView(R.id.tilInputLogIn)
+    TextInputLayout tilInputLogIn;
+    @BindView(R.id.tietInputLogIn)
+    TextInputEditText tietInputLogIn;
+    @BindView(R.id.tilPassword)
+    TextInputLayout tilPassword;
+    @BindView(R.id.tietPassword)
+    TextInputEditText tietPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,64 @@ public class IGLoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         imvBack.setOnClickListener(view -> finish());
+
+        tietPassword.setOnEditorActionListener((v, actionId, event) -> {
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+
+                tryLogin();
+
+            }
+            return false;
+        });
+
+        llLoginButton.setOnClickListener(v -> tryLogin());
+
+    }
+
+    private void tryLogin() {
+
+        hideKeyboard(IGLoginActivity.this);
+        if(validateInput()) {
+
+        }
+
+    }
+
+    private boolean validateInput() {
+
+        boolean validData = true;
+
+        String inputData = Objects.requireNonNull(tietInputLogIn.getText()).toString().trim();
+        String password = Objects.requireNonNull(tietPassword.getText()).toString().trim();
+
+        if (inputData.isEmpty()) {
+
+            tilInputLogIn.setErrorEnabled(true);
+            tilInputLogIn.setError("Empty field");
+            validData = false;
+
+        } else {
+
+            tilInputLogIn.setErrorEnabled(false);
+            validData = true;
+
+        }
+
+        if (password.isEmpty()) {
+
+            tilPassword.setErrorEnabled(true);
+            tilPassword.setError("Empty field");
+            validData = false;
+
+        } else {
+
+            tilPassword.setErrorEnabled(false);
+            validData = true;
+
+
+        }
+
+        return validData;
 
     }
 
