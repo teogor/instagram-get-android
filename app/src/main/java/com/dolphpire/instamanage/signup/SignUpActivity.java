f10a0889e39277f69482350baee1be0b33ed2a1e
Teodor G <grigor.teodor@gmail.com>
2020-06-07 22:08:58 +0200

    improved sign up techniques

--- app/src/main/java/com/dolphpire/instamanage/signup/SignUpActivity.java
+++ app/src/main/java/com/dolphpire/instamanage/signup/SignUpActivity.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dolphpire.android.material.textfield.TextInputEditText;
 import com.dolphpire.android.material.textfield.TextInputLayout;
 import com.dolphpire.api.action.user.check.DataCheckAction;
 import com.dolphpire.api.initializer.DolphPireApp;
+import com.dolphpire.api.interfaces.ApiCallback;
+import com.dolphpire.api.interfaces.FailureCallback;
 import com.dolphpire.instamanage.R;
 import com.dolphpire.instamanage.login.LoginActivity;
 
@@ -23,7 +28,8 @@ import java.util.Objects;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity
+{
 
     @BindView(R.id.tilInputEmail)
     TextInputLayout tilInputEmail;
@@ -49,7 +55,8 @@ public class SignUpActivity extends AppCompatActivity {
     RelativeLayout rlLoading;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
@@ -57,95 +64,142 @@ public class SignUpActivity extends AppCompatActivity {
 
         rlLoading.setVisibility(View.GONE);
 
-        llLogin.setOnClickListener(new View.OnClickListener() {
+        llLogin.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        llCreateAccount.setOnClickListener(v -> {
+        tietInputPasswordC.setOnEditorActionListener((v, actionId, event) ->
+        {
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE))
+            {
 
-            rlLoading.setVisibility(View.VISIBLE);
-            if (validateInput()) {
-                DolphPireApp.initializeApi().signup().createAccount()
-                        .setPassword(Objects.requireNonNull(tietInputPassword.getText()).toString())
-                        .setUsername(Objects.requireNonNull(tietInputUsername.getText()).toString())
-                        .setEmail(Objects.requireNonNull(tietInputEmail.getText()).toString())
-                        .set()
-                        .addOnCompleteListener(() -> {
-                            rlLoading.setVisibility(View.GONE);
-                            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
-                        })
-                        .addOnFailureListener(e -> rlLoading.setVisibility(View.GONE))
-                        .execute();
-            } else {
-                rlLoading.setVisibility(View.GONE);
+                tryRegister();
+
             }
+            return false;
+        });
+
+        llCreateAccount.setOnClickListener(v ->
+        {
+            tryRegister();
         });
 
         DataCheckAction usernameCheck = DolphPireApp.initializeApi().user().check();
         DataCheckAction emailCheck = DolphPireApp.initializeApi().user().check();
 
-        tietInputUsername.addTextChangedListener(new TextWatcher() {
+        tietInputUsername.addTextChangedListener(new TextWatcher()
+        {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
                 usernameCheck.username(s.toString())
-                        .addOnFoundListener(found -> {
-                            if(found) {
+                        .addOnFoundListener(found ->
+                        {
+                            if (found)
+                            {
                                 tilInputUsername.setErrorEnabled(true);
                                 tilInputUsername.setError("Username already exists");
-                            } else {
+                            } else
+                            {
                                 tilInputUsername.setErrorEnabled(true);
                             }
                         })
                         .execute();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
             }
         });
 
-        tietInputEmail.addTextChangedListener(new TextWatcher() {
+        tietInputEmail.addTextChangedListener(new TextWatcher()
+        {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
                 emailCheck.username(s.toString())
-                        .addOnFoundListener(found -> {
-                            if(found) {
+                        .addOnFoundListener(found ->
+                        {
+                            if (found)
+                            {
                                 tilInputEmail.setErrorEnabled(true);
                                 tilInputEmail.setError("Email already exists");
-                            } else {
+                            } else
+                            {
                                 tilInputEmail.setErrorEnabled(true);
                             }
                         })
                         .execute();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
             }
         });
 
     }
 
-    private boolean validateInput() {
+    private void tryRegister()
+    {
+
+        rlLoading.setVisibility(View.VISIBLE);
+        if (validateInput())
+        {
+            DolphPireApp.initializeApi().signup().createAccount()
+                    .setPassword(Objects.requireNonNull(tietInputPassword.getText()).toString())
+                    .setUsername(Objects.requireNonNull(tietInputUsername.getText()).toString())
+                    .setEmail(Objects.requireNonNull(tietInputEmail.getText()).toString())
+                    .set()
+                    .addOnCompleteListener(() ->
+                    {
+                        rlLoading.setVisibility(View.GONE);
+                        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    })
+                    .addOnFailureListener(e ->
+                    {
+                        rlLoading.setVisibility(View.GONE);
+                        Toast.makeText(SignUpActivity.this, "check the data entered and try again", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailedListener(() ->
+                    {
+                        rlLoading.setVisibility(View.GONE);
+                        Toast.makeText(SignUpActivity.this, "check the data entered and try again", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailureListener(e -> rlLoading.setVisibility(View.GONE))
+                    .execute();
+        } else
+        {
+            rlLoading.setVisibility(View.GONE);
+        }
+
+    }
+
+    private boolean validateInput()
+    {
 
         boolean validData = true;
 
@@ -154,55 +208,64 @@ public class SignUpActivity extends AppCompatActivity {
         String password = Objects.requireNonNull(tietInputPassword.getText()).toString().trim();
         String passwordC = Objects.requireNonNull(tietInputPasswordC.getText()).toString().trim();
 
-        if (email.isEmpty()) {
+        if (email.isEmpty())
+        {
 
             tilInputEmail.setErrorEnabled(true);
             tilInputEmail.setError("Empty field");
             validData = false;
 
-        } else {
+        } else
+        {
 
             tilInputEmail.setErrorEnabled(false);
 
         }
 
-        if (username.isEmpty()) {
+        if (username.isEmpty())
+        {
 
             tilInputUsername.setErrorEnabled(true);
             tilInputUsername.setError("Empty field");
             validData = false;
 
-        } else {
+        } else
+        {
 
             tilInputUsername.setErrorEnabled(false);
 
         }
 
-        if (password.isEmpty()) {
+        if (password.isEmpty())
+        {
 
             tilInputPassword.setErrorEnabled(true);
             tilInputPassword.setError("Empty field");
             validData = false;
 
-        } else {
+        } else
+        {
 
             tilInputPassword.setErrorEnabled(false);
 
         }
 
-        if (passwordC.isEmpty()) {
+        if (passwordC.isEmpty())
+        {
 
             tilInputPasswordC.setErrorEnabled(true);
             tilInputPasswordC.setError("Empty field");
             validData = false;
 
-        } else {
+        } else
+        {
 
             tilInputPasswordC.setErrorEnabled(false);
 
         }
 
-        if (!password.isEmpty() && !passwordC.isEmpty() && !password.equals(passwordC)) {
+        if (!password.isEmpty() && !passwordC.isEmpty() && !password.equals(passwordC))
+        {
 
             tilInputPassword.setErrorEnabled(true);
             tilInputPassword.setError("Passwords don't match");
@@ -217,7 +280,8 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         super.onBackPressed();
         Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
         startActivity(intent);
@@ -225,13 +289,15 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart()
+    {
         super.onStart();
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
