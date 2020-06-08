29cba0f6724ae57b68b7f49bc96c319c76c80d43
Teodor G <grigor.teodor@gmail.com>
2020-06-08 16:12:15 +0200

    removed unwanted directories

--- api_ig_get/src/main/java/com/dolphpire/api/initializer/DolphPireAPI.java
+++ api_ig_get/src/main/java/com/dolphpire/api/initializer/DolphPireAPI.java
@@ -1,9 +1,5 @@
 package com.dolphpire.api.initializer;
 
-import com.dolphpire.api.action.autocomplete.AutocompleteAction;
-import com.dolphpire.api.action.comments.CommentsAction;
-import com.dolphpire.api.action.discover.DiscoverAction;
-import com.dolphpire.api.action.hashtag.HashtagAction;
 import com.dolphpire.api.action.igaccount.IGAccountAction;
 import com.dolphpire.api.action.login.LoginAction;
 import com.dolphpire.api.action.notifications.Notifications;
@@ -45,33 +41,9 @@ public class DolphPireAPI
     }
 
     @KeepForApi
-    public AutocompleteAction autocomplete()
-    {
-        return new AutocompleteAction();
-    }
-
-    @KeepForApi
-    public HashtagAction hashtag()
-    {
-        return new HashtagAction();
-    }
-
-    @KeepForApi
-    public CommentsAction comments()
-    {
-        return new CommentsAction();
-    }
-
-    @KeepForApi
     public Notifications notifications()
     {
         return new Notifications();
-    }
-
-    @KeepForApi
-    public DiscoverAction discover()
-    {
-        return new DiscoverAction();
     }
 
 }
