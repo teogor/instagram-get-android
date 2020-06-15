4de780f0e71c2393682c7c300a07ab7ccf8b789d
Teodor G <grigor.teodor@gmail.com>
2020-06-15 19:35:44 +0200

    created order parser

---
+++ app/src/main/java/com/dolphpire/instamanage/ordersFragment/itemParser/adapter/AdapterOrders.java
@@ -0,0 +1,61 @@
+package com.dolphpire.instamanage.ordersFragment.itemParser.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dolphpire.api.models.IGPostModel;
+import com.dolphpire.instamanage.R;
+import com.dolphpire.instamanage.ordersFragment.itemParser.holder.HolderOrder;
+import com.dolphpire.instamanage.ordersFragment.itemParser.model.ModelOrder;
+
+import java.util.ArrayList;
+
+public class AdapterOrders extends RecyclerView.Adapter<HolderOrder> {
+
+    private Activity activity;
+    private ArrayList<ModelOrder> mDataList;
+    private OnItem listener;
+
+    public AdapterOrders(ArrayList<ModelOrder> mDataList, Activity activity) {
+        this.mDataList = mDataList;
+        this.activity = activity;
+    }
+
+    public OnItem getListener() {
+        return listener;
+    }
+
+    public void setListener(OnItem listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public HolderOrder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order, parent, false);
+        return new HolderOrder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(HolderOrder holder, int position) {
+        holder.setContent(mDataList.get(position), listener, position, activity);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    public interface OnItem {
+        void onSelected(int pos);
+    }
+
+
+}
