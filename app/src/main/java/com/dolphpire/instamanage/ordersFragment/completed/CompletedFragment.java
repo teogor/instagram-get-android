95f5814d1b4126f4a7f5244e2f727fb2ba94d65c
Teodor G <grigor.teodor@gmail.com>
2020-06-15 22:40:33 +0200

    added completed & in progress orders fragment

--- app/src/main/java/com/dolphpire/instamanage/ordersFragment/completed/CompletedFragment.java
+++ app/src/main/java/com/dolphpire/instamanage/ordersFragment/completed/CompletedFragment.java
@@ -10,9 +10,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.dolphpire.api.initializer.DolphPireApp;
+import com.dolphpire.api.interfaces.OnOrdersRetrieved;
+import com.dolphpire.api.models.OrderModel;
 import com.dolphpire.instamanage.R;
+import com.dolphpire.instamanage.ordersFragment.itemParser.adapter.AdapterOrders;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +34,10 @@ public class CompletedFragment extends Fragment
     private View mView;
     private Context mContext;
     private Activity mActivity;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<OrderModel> mDataList;
+    private AdapterOrders mAdapter;
+    private OrderModel mOrderModel;
 
     public CompletedFragment()
     {
@@ -71,6 +84,33 @@ public class CompletedFragment extends Fragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
+
+        mDataList = new ArrayList<>();
+        mAdapter = new AdapterOrders(mDataList, mActivity);
+
+        rvOrders.setItemAnimator(new DefaultItemAnimator());
+        linearLayoutManager = new LinearLayoutManager(mActivity);
+        rvOrders.setLayoutManager(linearLayoutManager);
+        rvOrders.setHasFixedSize(false);
+        rvOrders.setAdapter(mAdapter);
+
+        populateRecyclerView();
+
+    }
+
+    private void populateRecyclerView()
+    {
+
+        DolphPireApp.initializeApi()
+                .orders()
+                .retrieve()
+                .completed()
+                .addOnCompleteListener(dataList ->
+                {
+                    mDataList.addAll(dataList);
+                    mAdapter.notifyDataSetChanged();
+                })
+                .execute();
 
     }
 
