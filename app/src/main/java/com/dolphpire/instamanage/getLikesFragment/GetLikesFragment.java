57359832bfe8d99e6a614174ecef8ae3a67f0f23
Teodor G <grigor.teodor@gmail.com>
2020-06-18 22:18:59 +0200

    added log info

--- app/src/main/java/com/dolphpire/instamanage/getLikesFragment/GetLikesFragment.java
+++ app/src/main/java/com/dolphpire/instamanage/getLikesFragment/GetLikesFragment.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
